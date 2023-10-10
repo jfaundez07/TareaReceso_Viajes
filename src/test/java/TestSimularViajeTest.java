d85fa7bd79956d27bca89dc476aba90cb54916c7
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-09 22:52:20 -0300

    Commit creacion unitTest y actualizacion pom

---
+++ src/test/java/TestSimularViajeTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSimularViajeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void crearVehículo() {
+    }
+
+    @Test
+    void crearPersona() {
+    }
+
+    @Test
+    void isMayorCero() {
+    }
+}
