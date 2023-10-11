8053ff977579c3b1a95c1c920afa54edda16e6c7
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-11 15:41:16 -0300

    Commit pruebas crearVehiculo, crearPersona, isMayorCero.

--- src/test/java/TestSimularViajeTest.java
+++ src/test/java/TestSimularViajeTest.java
@@ -6,23 +6,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TestSimularViajeTest {
 
+    TestSimularViaje objetoTestSimularViaje;
+    Persona objetoPersona;
+    Vehículo obejetoVehiculo;
+
     @BeforeEach
     void setUp() {
+        objetoTestSimularViaje = new TestSimularViaje();
+        objetoPersona = new Persona("Joaquin");
+        obejetoVehiculo = new Vehículo("Auto", 50);
     }
 
     @AfterEach
     void tearDown() {
+        objetoTestSimularViaje = null;
+        objetoPersona = null;
+        obejetoVehiculo = null;
     }
 
     @Test
     void crearVehículo() {
+        assertEquals(obejetoVehiculo.getClass(), Vehículo.class);
+
     }
 
     @Test
     void crearPersona() {
+        assertEquals(objetoPersona.getClass(), Persona.class);
     }
 
     @Test
     void isMayorCero() {
+        assertTrue(objetoTestSimularViaje.isMayorCero(0.00001));
+        assertFalse(objetoTestSimularViaje.isMayorCero(-0.0001));
     }
+
+
 }
